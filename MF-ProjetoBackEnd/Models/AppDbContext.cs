38399a3ba8b2cf130f8e046d53a6574956f84c63
gabriel mart <gabrielmart1309@gmail.com>
2026-04-25 20:35:44 -0300

    Configurando banco de dados e da tabela veiculo

---
+++ MF-ProjetoBackEnd/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MF_ProjetoBackEnd.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
---
+++ MF-ProjetoBackEnd/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MF_ProjetoBackEnd.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do veículo é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A placa do veículo é obrigatoria.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O ano de fabricação do veículo é obrigatório.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O ano do modelo do veículo é obrigatório.")]
+        public int AnoModelo { get; set; }
+    }
+}
